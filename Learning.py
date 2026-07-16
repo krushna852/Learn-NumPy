0b9b315b6b8882ca7fc47dd7b14de1524e00e822
krushna852 <krushnawalunjkar11@gmail.com>
2026-07-16 20:03:22 +0530

    List VS Numoy array

--- Learning.py
+++ Learning.py
@@ -1,9 +1,15 @@
 import time 
+import numpy as np
 
 start = time.time()
-i=1
-li=[]
-while i <=100000000:
-    li=i
-    i+=1
+li=[1,2,3,4,5,6,7,8,9,10]*1000
+li2=[]
+for i in li:
+    li2.append(i*2)
 print("Total Time required is",time.time()-start)
+
+new_start=time.time()
+li1=[1,2,3,4,5,6,7,8,9,10]*1000
+a=np.array(li1)
+new_a=a*2
+print("Total Time required is",time.time()-new_start)

b6975bd7ca2c95f0808f24638f5fd3e143e3558d
krushna852 <krushnawalunjkar11@gmail.com>
2026-07-17 17:27:36 +0530

    Dimensions of Array

--- Learning.py
+++ Learning.py
@@ -1,7 +1,16 @@
 import numpy as np
-import sys
 
-a=[1,2,3,4,5,6,7,8,9,10]
-b=np.array(a)
-print("size of list is", sys.getsizeof(a))
-print("size of array is", b.nbytes)
+arr=np.array(32)
+print(arr.ndim)
+
+arr=np.array([32])
+print(arr.ndim)
+
+arr=np.array([1,2,3,4])
+print(arr.ndim)
+
+a=np.array([[123],[125],[564]])
+print(a.ndim)
+
+b=np.array([[[1,2],[2,9]]])
+print(b.ndim)

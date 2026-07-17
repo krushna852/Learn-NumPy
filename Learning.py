adb989098901c3cf33446f1a5821d8ac2b1bfdd0
krushna852 <krushnawalunjkar11@gmail.com>
2026-07-17 18:13:15 +0530

    Multiple ways to create different area

--- Learning.py
+++ Learning.py
@@ -5,3 +5,32 @@ print(np.array(li))
 
 a=np.arange(10,1,-1) # start stop step
 print(a)
+
+b=np.linspace(0,1,3) # (start,ending, n-> is n numbers between  start and end)
+print(b)
+
+c=np.logspace(1,3,2) #logrithmetic scale 10^1,10^2,10^3 from this only 2 
+print(c)
+
+d=np.zeros(4) # create a array of zeroes
+print(d)
+
+#Matrix Using Array
+e=np.zeros([2,3])
+print(e)
+
+
+#Array of ones
+a=np.ones([3,4],dtype=str)
+print(a)
+
+#array of Any one Random Number
+b=np.full(5,7) # (number of value, value)
+print(b)
+
+b=np.full([5,3],9) # (pass Dimension [rows , columns] , Value)
+print(b)
+
+#Empty Array
+a=np.empty([2,3]) #([row , cloums])
+print(a)

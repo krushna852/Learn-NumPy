4f0cd423eadb6ae8bf5bed599e64f50139314ad2
krushna852 <krushnawalunjkar11@gmail.com>
2026-07-17 17:45:38 +0530

    Creating Arrays

--- Learning.py
+++ Learning.py
@@ -1,16 +1,7 @@
 import numpy as np
 
-arr=np.array(32)
-print(arr.ndim)
+li=[1,2,3,4,5,6]
+print(np.array(li))
 
-arr=np.array([32])
-print(arr.ndim)
-
-arr=np.array([1,2,3,4])
-print(arr.ndim)
-
-a=np.array([[123],[125],[564]])
-print(a.ndim)
-
-b=np.array([[[1,2],[2,9]]])
-print(b.ndim)
+a=np.arange(10,1,-1) # start stop step
+print(a)

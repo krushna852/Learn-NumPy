f292e58676dd24c4fc62bf71acf74459b26e6f76
krushna852 <krushnawalunjkar11@gmail.com>
2026-07-18 17:19:33 +0530

    Type Casting

--- Learning.py
+++ Learning.py
@@ -22,3 +22,17 @@ print(type(a[1]))
 a=np.array([56,"hdvdb",56.12,True,"455"])
 print(a)
 print(type(a[1]))
+
+#Type Casting
+a=np.array([12,34,5,67])
+a=a.astype(np.float64)
+print(a)
+
+b=np.array(["12","8","45","4"])
+b=b.astype(np.int64)
+print(b)
+
+a=np.array([23.01,56.89,56.99,25.1,69.9999])
+a=a.astype(np.str_)
+print(a)
+

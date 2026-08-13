bcf648a953417544fccb5f9ec1e1617a6d067e2b
krushna852 <krushnawalunjkar11@gmail.com>
2026-08-13 16:47:57 +0530

    Repeating and Tiling

--- Learning.py
+++ Learning.py
@@ -1,19 +1,8 @@
 import numpy as np
 
-#Remark np.info and .nbytes
+#Repeating :- Repeats Each element
+arr=np.array([1,2,3])
+print(np.repeat(arr,3))
 
-a=np.arange(1,10)
-print(a.nbytes) # it returns memory of an array
-print(a.itemsize)
-
-print(np.info(np.astype)) # it returns documentation or information 
-
-# Spliting Of an Array
-
-arr=np.arange(1,9)
-print(np.split(arr,2)) # if equal divison
-
-print(np.hsplit(arr,4)) # splits horizontally
-
-a=np.arange(1,10).reshape(3,3)
-print(np.vsplit(a,3))  # splits vertically only above 2 or 2d array
+#tile -> Repeats Whole Array
+print(np.tile(arr,3))

3fb6530304785031f99cb2443b8f075060d9fdf1
krushna852 <krushnawalunjkar11@gmail.com>
2026-08-13 17:22:11 +0530

    Aggregate Functions

--- Learning.py
+++ Learning.py
@@ -1,8 +1,33 @@
 import numpy as np
 
-#Repeating :- Repeats Each element
-arr=np.array([1,2,3])
-print(np.repeat(arr,3))
+#Aggregate Functions
 
-#tile -> Repeats Whole Array
-print(np.tile(arr,3))
+#For 1D Array
+
+a=np.arange(1,10)
+
+print(np.sum(a))
+
+print(np.mean(a))
+
+print(np.median(a))
+
+print(np.var(a))
+
+print(np.std(a))
+
+print(np.min(a))
+
+print(np.max(a))
+
+#for 2D Array
+
+a=a.reshape(3,3)
+
+print(a)
+
+print(np.sum(a,axis =0)) # sum of all elements in Single column
+
+print(np.sum(a, axis=1)) #sum all elements in single row
+
+print(np.min(a))

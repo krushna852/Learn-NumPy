cae97ab2853a6c015734f95de368e7d4e351c1b0
krushna852 <krushnawalunjkar11@gmail.com>
2026-08-17 18:28:59 +0530

    Dealing With Missing Values

--- Learning.py
+++ Learning.py
@@ -1,12 +1,26 @@
 import numpy as np
 
+#Dealing with Missing Values
 
-#Vectorization
+#np.nan -> not a number
+arr=np.array([1,8,9,np.nan,2,6])
+print(arr)
 
-def square(x):
-    return x*x
+#np.inf and -np.inf -> positive and negative Infinite
 
-squarev=np.vectorize(square)  # it Convert normsl function into numpy function
+a=np.array([np.inf,56,23,np.inf,np.nan])
+print(a)
 
-a=np.array([1,2,3,4,5,6])
-print(squarev(a))
+c=np.arange(1,10)
+#special function for this
+
+print(np.isnan(arr).any())
+
+print(np.isinf(a).any())  
+
+print(np.isfinite(c).any())
+
+#removing nan and infinite value
+
+new=np.nan_to_num(a)
+print(new)
